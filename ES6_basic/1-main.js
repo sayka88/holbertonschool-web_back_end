06627ab03993688328f13af0e31f5de9dbe14d82
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-19 14:48:09 +0400

    Update 1-main.js

--- ES6_basic/1-main.js
+++ ES6_basic/1-main.js
@@ -1,4 +1,4 @@
-// babel.config.js
-module.exports = {
-  presets: ['@babel/preset-env'],
-};
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
