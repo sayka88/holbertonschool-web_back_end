394a48eb1eb7db6bb269b6f4d4fa7c58d976efa2
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-22 17:27:24 +0400

    Update 4-main.js

--- ES6_data_manipulation/4-main.js
+++ ES6_data_manipulation/4-main.js
@@ -1,6 +1,14 @@
-import getListStudents from "./0-get_list_students.js";
-import updateStudentGradeByCity from "./4-update_grade_by_city.js";
+import getListStudents from './0-get_list_students.js';
+import updateStudentGradeByCity from './4-update_grade_by_city.js';
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+// [
+//   { id: 1, firstName: 'Guillaume', location: 'San Francisco', grade: 86 },
+//   { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
+// ]
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+// [
+//   { id: 1, firstName: 'Guillaume', location: 'San Francisco', grade: 'N/A' },
+//   { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
+// ]
