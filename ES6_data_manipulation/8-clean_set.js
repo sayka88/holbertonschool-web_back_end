d857ea3365d220669af5892f44fe21bb906167ac
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-22 17:43:16 +0400

    Create 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
