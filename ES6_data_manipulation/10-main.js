9da578192933adcba1c69ece397f7b6f4dd41dde
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-22 17:48:11 +0400

    Update 10-main.js

--- ES6_data_manipulation/10-main.js
+++ ES6_data_manipulation/10-main.js
@@ -1,14 +1,8 @@
-Map {
-  'Apples' => 10,
-  'Tomatoes' => 10,
-  'Pasta' => 1,
-  'Rice' => 1,
-  'Banana' => 5
-}
-Map {
-  'Apples' => 10,
-  'Tomatoes' => 10,
-  'Pasta' => 100,
-  'Rice' => 100,
-  'Banana' => 5
-}
+import updateUniqueItems from './10-update_uniq_items.js';
+import groceriesList from './9-groceries_list.js';
+
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map);
+console.log(map);
