3096bd35bc6db7f0de1028b6573a68b80117cb63
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-22 17:47:11 +0400

    Create 10-main.js

---
+++ ES6_data_manipulation/10-main.js
@@ -0,0 +1,14 @@
+Map {
+  'Apples' => 10,
+  'Tomatoes' => 10,
+  'Pasta' => 1,
+  'Rice' => 1,
+  'Banana' => 5
+}
+Map {
+  'Apples' => 10,
+  'Tomatoes' => 10,
+  'Pasta' => 100,
+  'Rice' => 100,
+  'Banana' => 5
+}
