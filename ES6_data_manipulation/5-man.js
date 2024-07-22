4d181979718a8bc753156c97eefcedf79f97b4ba
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-22 17:28:31 +0400

    Create 5-man.js

---
+++ ES6_data_manipulation/5-man.js
@@ -0,0 +1,4 @@
+import createInt8TypedArray from './5-typed_arrays.js';
+
+console.log(createInt8TypedArray(10, 2, 89));
+// DataView { byteLength: 10, byteOffset: 0, buffer: ArrayBuffer { byteLength: 10 } }
