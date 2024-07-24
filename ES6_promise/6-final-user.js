709f3c9e6051289fa5f545cbf358434fdc887e84
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-24 15:34:03 +0400

    Create 6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: result.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  });
+}
