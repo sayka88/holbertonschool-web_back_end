974ca4706202483b1636fc3f1fe2a3d5d9dd754e
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-24 15:39:29 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,15 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-  const results = await Promise.allSettled([userPromise, photoPromise]);
-
-  return results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: 'fulfilled',
-        value: result.value,
-      };
-    }
-    return {
-      status: 'rejected',
-      value: result.reason,
-    };
-  });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      return results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return { status: 'fulfilled', value: result.value };
+        } else {
+          return { status: 'rejected', value: result.reason };
+        }
+      });
+    });
 }
