d82300509304a203d1ae82382d355851ce316386
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-07-24 15:54:08 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) =>
-      result.status === 'fulfilled'
-        ? { status: 'fulfilled', value: result.value }
-        : { status: 'rejected', value: result.reason.toString() }
-    ));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const res = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return res.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}`,
+  }));
 }
