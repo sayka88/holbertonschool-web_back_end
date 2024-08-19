466129af40ce9a3586293682d983fadc37c50ee8
sayka88 <sayka.alekperova@gmail.com>
2024-08-19 10:09:47 +0000

    0-main.py

--- python_async_comprehension/0-main.py
+++ python_async_comprehension/0-main.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
-
 import asyncio
+import random
+from typing import AsyncGenerator
 
-async_generator = __import__('0-async_generator').async_generator
+async def async_generator() -> AsyncGenerator[float, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
 
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-asyncio.run(print_yielded_values())
